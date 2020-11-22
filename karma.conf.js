1d783b48b3dde6fcdf3aa2650bace3c691b44cd2
Adrien RICHARD <richardadrien0@gmail.com>
2020-11-22 17:35:38 +0100

    fixed github CI

--- karma.conf.js
+++ karma.conf.js
@@ -27,6 +27,12 @@ module.exports = function (config) {
     logLevel: config.LOG_INFO,
     autoWatch: true,
     browsers: ['Chrome','Firefox'],
+    customLaunchers: {
+      ChromeHeadlessCI: {
+        base: 'ChromeHeadless',
+        flags: ['--no-sandbox']
+      }
+    },
     singleRun: false,
     restartOnFileChange: true
   });
